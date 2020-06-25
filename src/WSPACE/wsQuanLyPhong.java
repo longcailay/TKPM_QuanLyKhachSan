f05704c08b150cb3b2c8e0762559831bc1add7b9
longcailay <longcailay@gmail.com>
2020-06-25 10:50:26 +0700

    Hoàn thành form QuanLyPhong

--- src/WSPACE/wsQuanLyPhong.java
+++ src/WSPACE/wsQuanLyPhong.java
@@ -23,6 +23,15 @@ import java.awt.Dimension;
 import javax.swing.border.BevelBorder;
 
 import UI.HomePage;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.JEditorPane;
+import javax.swing.BoxLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import java.awt.GridLayout;
 
 public class wsQuanLyPhong extends JPanel {
 	/*Khai báo các biến*/
@@ -30,6 +39,10 @@ public class wsQuanLyPhong extends JPanel {
 	public static JComboBox cmbTinhTrang;
 	public static JButton btnDuyet;
 	public static JButton btnThemPhongMoi;
+	private JTextField txtTinhTrang;
+	private JTextField txtNgayThue;
+	private JTable table;
+	private JTextField textField;
 	/**
 	 * Create the panel.
 	 */
@@ -47,9 +60,9 @@ public class wsQuanLyPhong extends JPanel {
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 586, Short.MAX_VALUE)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 542, Short.MAX_VALUE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 226, GroupLayout.PREFERRED_SIZE)
+					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 270, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
@@ -57,13 +70,89 @@ public class wsQuanLyPhong extends JPanel {
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(panel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(panel, GroupLayout.DEFAULT_SIZE, 622, Short.MAX_VALUE)
 						.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE, Short.MAX_VALUE))
 					.addContainerGap())
 		);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("PHÒNG");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_1.setBounds(69, 57, 75, 20);
+		panel_2.add(lblNewLabel_1);
+		
+		JLabel lblPhong = new JLabel("New label");
+		lblPhong.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblPhong.setBounds(154, 57, 45, 20);
+		panel_2.add(lblPhong);
+		
+		JLabel lblNewLabel_3 = new JLabel("Tình trạng");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_3.setBounds(42, 139, 75, 13);
+		panel_2.add(lblNewLabel_3);
+		
+		txtTinhTrang = new JTextField();
+		txtTinhTrang.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtTinhTrang.setEnabled(false);
+		txtTinhTrang.setBounds(127, 137, 96, 19);
+		panel_2.add(txtTinhTrang);
+		txtTinhTrang.setColumns(10);
+		
+		JLabel lblNgayThue = new JLabel("Tình trạng");
+		lblNgayThue.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNgayThue.setBounds(42, 192, 75, 13);
+		panel_2.add(lblNgayThue);
+		
+		txtNgayThue = new JTextField();
+		txtNgayThue.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtNgayThue.setEnabled(false);
+		txtNgayThue.setColumns(10);
+		txtNgayThue.setBounds(127, 190, 96, 19);
+		panel_2.add(txtNgayThue);
+		
+		JLabel lblKhchHng = new JLabel("Khách hàng");
+		lblKhchHng.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblKhchHng.setBounds(42, 246, 75, 13);
+		panel_2.add(lblKhchHng);
+		
+		table = new JTable();
+		table.setBounds(10, 269, 250, 115);
+		panel_2.add(table);
+		
+		JLabel lblGhiCh = new JLabel("Ghi chú");
+		lblGhiCh.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblGhiCh.setBounds(42, 411, 75, 13);
+		panel_2.add(lblGhiCh);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 434, 250, 63);
+		panel_2.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnThuePhong = new JButton("Thuê phòng");
+		btnThuePhong.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnThuePhong.setBounds(10, 516, 117, 33);
+		panel_2.add(btnThuePhong);
+		
+		JButton btnTraPhong = new JButton("Trả phòng");
+		btnTraPhong.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnTraPhong.setBounds(143, 516, 117, 33);
+		panel_2.add(btnTraPhong);
+		
+		JButton btnSuaPhong = new JButton("Sửa phòng");
+		btnSuaPhong.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnSuaPhong.setBounds(10, 579, 117, 33);
+		panel_2.add(btnSuaPhong);
+		
+		JButton btnXoaPhong = new JButton("Xóa phòng");
+		btnXoaPhong.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnXoaPhong.setBounds(143, 579, 117, 33);
+		panel_2.add(btnXoaPhong);
 		
 		JPanel panel_1 = new JPanel();
 		JPanel panel_3 = new JPanel();
+		panel_3.setMinimumSize(new Dimension(15, 15));
+		panel_3.setForeground(new Color(0, 0, 0));
 		panel_3.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		panel_3.setAutoscrolls(true);
 		panel_3.setAlignmentX(Component.LEFT_ALIGNMENT);
@@ -74,6 +163,7 @@ public class wsQuanLyPhong extends JPanel {
 				.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 586, Short.MAX_VALUE)
 				.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 586, Short.MAX_VALUE)
 		);
+		
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.BASELINE)
 				.addGroup(gl_panel.createSequentialGroup()
@@ -82,6 +172,8 @@ public class wsQuanLyPhong extends JPanel {
 		);
 		
 		
+		
+		//load danh sách các phòng
 		for(int i = 0; i < 30; i++) {
 		JButton btn = new JButton("Button" + i);
 		btn.setPreferredSize(new Dimension(108, 100));//Kích thước chuẩn
@@ -158,34 +250,6 @@ public class wsQuanLyPhong extends JPanel {
 				HomePage.lblTitle.setText(btnThemPhongMoi.getText());
 			}
 		});
-		
-		JLabel lblNewLabel_3 = new JLabel("Danh S\u00E1ch Kh\u00E1ch Thu\u00EA");
-		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBackground(Color.RED);
-		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
-		gl_panel_2.setHorizontalGroup(
-			gl_panel_2.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_2.createSequentialGroup()
-					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel_2.createSequentialGroup()
-							.addGap(241)
-							.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_panel_2.createSequentialGroup()
-							.addGap(53)
-							.addComponent(btnNewButton)))
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		);
-		gl_panel_2.setVerticalGroup(
-			gl_panel_2.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_2.createSequentialGroup()
-					.addGap(24)
-					.addComponent(lblNewLabel_3)
-					.addGap(116)
-					.addComponent(btnNewButton)
-					.addContainerGap(448, Short.MAX_VALUE))
-		);
-		panel_2.setLayout(gl_panel_2);
 		setLayout(groupLayout);
 
 	}
